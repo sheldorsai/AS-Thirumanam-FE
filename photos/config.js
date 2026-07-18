fa7eead70a55b55b2a1b9a0612cda2837e1bd9b4
srikumarma <srikumarma1201@gmail.com>
2026-07-18 18:10:21 +0530

    updated main

--- photos/config.js
+++ photos/config.js
@@ -2,4 +2,4 @@
 //  PHOTOS PAGE PIN  —  edit the value below to change it
 //  Then save the file and push to GitHub.
 // ─────────────────────────────────────────────────────────
-var PHOTOS_PIN = "1234";
+var PHOTOS_PIN = "6402";
